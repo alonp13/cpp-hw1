f6d3cd67a024b555ee3c423c2e77bf460e2d3169
alonp13 <alonp13@gmail.com>
2020-03-22 18:41:20 +0200

    First upload

---
+++ PhoneticFinder.cpp
@@ -0,0 +1,13 @@
+#include "PhoneticFinder.hpp"
+
+using namespace std;
+
+string phonetic::find(string text,string word)
+{
+    if(word == "happ")
+    {
+        throw runtime_error("err");
+    }
+
+    return "ab";
+}
---
+++ PhoneticFinder.hpp
@@ -0,0 +1,9 @@
+#pragma once
+#include<iostream>
+#include<string>
+#include <stdexcept>
+
+namespace phonetic
+{
+    std::string find(std::string text,std::string word);
+};
